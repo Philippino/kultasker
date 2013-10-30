06d14d458af8c5839e2efe4a88ba498c219dfb04
Philip <PhilippinoPhil@gmail.com>
2013-10-30 18:02:56 +0400

    Добавлена функция сохранения введённых данных нового обхода, неработающая

--- task_viewer/views.py
+++ task_viewer/views.py
@@ -1,7 +1,24 @@
 # Create your views here.
 from task_viewer.models import *
+from task_viewer.forms import *
 from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
 
-def view_checks(request): #Вызов тыблицы шаблонов обходов
-	checks = Check.objects.all()
+def view_checks(request): #Вызов таблицы шаблонов обходов
+	checks = Check.objects.all() #Загрузка всех шаблонов обходов 
 	return render_to_response('checks.html', {'checks': checks,})
+
+
+def create_check(request): 							#Создание нового шаблона обхода
+	if request.method == 'POST': 						#Проверка, если запрос формата POST
+		chk = request.POST									#Извлечение запроса POST
+		chk_form = CheckForm(chk)							#Подставление данных запроса в форму
+		if chk_form.is_valid():         					#Если формат данных в форме правильный
+			new_chk = Check()									#Создается экземпляр класса Check
+			new_chk.name = chk['name']  						#В экземпляр записывается аргумент name
+			new_chk.save()										#Экземпляр сохраняется в базе данных
+			return HttpResponseRedirect('/checks/') 			#Форма перенаправляется на основную страницу
+	else:												#Если запрос формата GET
+		chk_form = CheckForm()								#Создание новой формы
+		return render_to_response('check_create.html', RequestContext(request, {'checks': chk_form}) ) #Формирование страницы с пустой формой
