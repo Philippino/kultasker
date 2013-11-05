8d7032d2806de577edd4a7723d43fae5cbeebff1
Philip <PhilippinoPhil@gmail.com>
2013-11-05 14:20:25 +0400

    Подключен bootstrap app

--- tasker/settings.py
+++ tasker/settings.py
@@ -121,6 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'task_viewer',
+    'bootstrap_toolkit',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
