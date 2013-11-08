4fd25347553b593b23993d1c149d062d6e3cf383
Philip <PhilippinoPhil@gmail.com>
2013-11-08 18:12:59 +0400

    Создана django-форма добавления заданий

--- task_viewer/forms.py
+++ task_viewer/forms.py
@@ -1,4 +1,9 @@
 from django import forms
+from task_viewer.models import Check
 
 class CheckForm(forms.Form):
-	name = forms.CharField(max_length = 100, label = 'Название')
+	name = forms.CharField(max_length = 100, label = 'Название шаблона обхода')
+
+class TaskForm(forms.Form):
+	check = forms.ModelChoiceField(label = 'Обход', queryset = Check.objects.all())
+	task = forms.CharField(max_length = 100, label = 'Задание обхода')
