10b2da52c4661c9a30603c01ba0ed7b58d25cb85
Philip <PhilippinoPhil@gmail.com>
2013-11-19 17:25:18 +0400

    Исправлен формат даты, если дата обхода была в прошлом году

--- task_viewer/templatetags/date_extras.py
+++ task_viewer/templatetags/date_extras.py
@@ -14,17 +14,24 @@ def format(given_date):
 	today = timezone.now()
 	delta = today - given_date
 	if today.day == given_date.day:
-		return "Сегодня в " + str(given_date.hour) + ':' + str(given_date.minute)
+		return "Сегодня" #str(given_date.hour) + ':' + str(given_date.minute)
 	elif delta.days == 1:
 		return "Вчера"
+	elif today.year != given_date.year:
+		return str(given_date.day) +' '+ get_month(given_date.month-1) +' '+ str(given_date.year)
+	elif delta.days > 40:
+		return str(given_date.day) +' '+ get_month(given_date.month-1)
+	elif delta.days >= 30:
+		return 'Месяц назад'
+	elif delta.days >= 21:
+		return 'Три недели назад'
+	elif delta.days >= 14:
+		return 'Две недели назад'
+	elif delta.days >= 7:
+		return 'Неделю назад'
 	elif delta.days > 1 and delta.days < 5:
 		return "%s дня назад" % delta.days
-	elif delta.days == 7:
-		return 'Неделю назад'
-	elif delta.days > 365:
-		return str(given_date.day) +' '+ get_month(given_date.month-1) +' '+ str(given_date.year)	
-	elif delta.days > 7:
-		return str(given_date.day) +' '+ get_month(given_date.month-1)
+	
 	elif delta.days > 1:
 		return "%s %s назад" % (abs(delta.days),("день" if abs(delta.days) == 1 else "дней"))
 
