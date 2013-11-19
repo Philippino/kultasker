3f07bf022052090026d8f7770c46de2533e41582
Philip <PhilippinoPhil@gmail.com>
2013-11-19 15:45:15 +0400

    Фильтрация дат доработана и теперь работает на всех страницах

--- task_viewer/templatetags/date_extras.py
+++ task_viewer/templatetags/date_extras.py
@@ -1,19 +1,33 @@
 # coding: utf-8
 from django import template
-from datetime import timedelta
+from datetime import timedelta, datetime
 from django.utils import timezone
 
 register = template.Library()
 
-@register.filter
-def format(given_date):
+def get_month(given_index):
 	months = list(['Января', 'Февраля', 'Марта', 'Апреля' , 'Мая' , 'Июня' , 'Июля' , 'Августа' , 'Сентября', 'Октября', 'Ноября',  'Декабря'])
+	return months[given_index]
+
+@register.filter(expects_localtime=True)
+def format(given_date):
+	
 	delta = timezone.now() - given_date
 	if delta.days == 0:
-		return "Сегодня в %s часов" % delta.seconds // 60
+		return "Сегодня " + str(delta.seconds//3600)+':'+ str((delta.seconds//60)%60)
 	elif delta.days == 1:
-		return "Вчера" 
+		return "Вчера " + str(delta.seconds//3600)+':'+ str((delta.seconds//60)%60)	
 	elif delta.days > 1 and delta.days < 5:
 		return "%s дня назад" % delta.days
+	elif delta.days == 7:
+		return 'Неделю назад'
+	elif delta.days > 365:
+		return str(given_date.day) +' '+ get_month(given_date.month-1) +' '+ str(given_date.year)	
+	elif delta.days > 7:
+		return str(given_date.day) +' '+ get_month(given_date.month-1)
 	elif delta.days > 1:
 		return "%s %s назад" % (abs(delta.days),("день" if abs(delta.days) == 1 else "дней"))
+
+@register.filter(expects_localtime=True)		
+def custom_format(given_date):
+	return str(given_date.day) +' '+ get_month(given_date.month-1) +' '+ str(given_date.year)	
