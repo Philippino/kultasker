a45a3160614576c536682c7ba81d284133730dd3
Philip <PhilippinoPhil@gmail.com>
2013-11-28 13:03:57 +0400

    Добавлено форматирование дат на "Позавчера"

--- task_viewer/templatetags/date_extras.py
+++ task_viewer/templatetags/date_extras.py
@@ -13,11 +13,7 @@ def get_month(given_index):
 def format(given_date):
 	today = timezone.now()
 	delta = today - given_date
-	if today.day == given_date.day and today.year == given_date.year and given_date.month == today.month:
-		return "Сегодня" #str(given_date.hour) + ':' + str(given_date.minute)
-	elif today.day - 1== given_date.day and today.year == given_date.year and given_date.month == today.month:
-		return "Вчера"
-	elif today.year != given_date.year:
+	if today.year != given_date.year:
 		return str(given_date.day) +' '+ get_month(given_date.month-1) +' '+ str(given_date.year)
 	elif delta.days > 40:
 		return str(given_date.day) +' '+ get_month(given_date.month-1)
@@ -31,9 +27,14 @@ def format(given_date):
 		return 'Неделю назад'
 	elif delta.days > 1 and delta.days < 5:
 		return "%s дня назад" % delta.days
-	
 	elif delta.days > 1:
 		return "%s %s назад" % (abs(delta.days),("день" if abs(delta.days) == 1 else "дней"))
+	elif today.day == given_date.day and today.year == given_date.year and given_date.month == today.month:
+		return "Сегодня" #str(given_date.hour) + ':' + str(given_date.minute)
+	elif today.day - 1 == given_date.day and today.year == given_date.year and given_date.month == today.month:
+		return "Вчера"
+	elif today.day - 2 == given_date.day and today.year == given_date.year and given_date.month == today.month:
+		return "Позавчера"
 
 @register.filter(expects_localtime=True)		
 def custom_format(given_date):
