7400a2833cebe36636f77dc26cf94c1498ed522a
Philip <PhilippinoPhil@gmail.com>
2013-12-03 12:54:34 +0400

    Создано хранилище статических файлов.

--- tasker/settings.py
+++ tasker/settings.py
@@ -68,7 +68,7 @@ STATIC_ROOT = ''
 STATIC_URL = '/static/'
 
 # Additional locations of static files
-STATICFILES_DIRS = ('../tasker/static/'
+STATICFILES_DIRS = ('../tasker/static/',
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
