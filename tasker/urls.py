cd47c852e15d2436ba1f6ddc8d3839e33a52a8a4
Anton <PhilippinoPhil@gmail.com>
2013-12-24 12:02:54 +0400

    Переведены комментарии к странице URL.

--- tasker/urls.py
+++ tasker/urls.py
@@ -8,31 +8,31 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$',index),
-    url(r'^accounts/login/$', login),
-    url(r'^accounts/logout/$', logout),
-    url(r'^accounts/details/$', account_details),
-    url(r'^accounts/details/password/change/$', password_change),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', login), #show login page, perform a login
+    url(r'^accounts/logout/$', logout), #perform a logout
+    url(r'^accounts/details/$', account_details), #show user profile
+    url(r'^accounts/details/password/change/$', password_change), #change user password
+    url(r'^admin/', include(admin.site.urls)), #show admin interface
 )
 
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += patterns('task_viewer',
-	url(r'^checks/$', 'views.view_checks'), #вызов таблицы шаблонов обходов
-    url(r'^checks/(?P<check>\d+)/dates/$', 'views.view_dates'), #показ дат обходов или создание нового обхода
-    url(r'^checks/(?P<check>\d+)/dates/new/$', 'views.new_date'), #создание новой даты
-    url(r'^checks/(?P<check>\d+)/tasks/$', 'views.view_tasks'), #показ заданий, связанных с шаблоном
-    url(r'^checks/(?P<check>\d+)/dates/(?P<date>\d+)/results/$', 'views.view_results'),  #показ результатов обхода
-    url(r'^checks/(?P<check>\d+)/dates/(?P<date>\d+)/results/(?P<result>\d+)/change/$', 'views.change_result'), #изменение статуса результата
-    url(r'^checks/(?P<check>\d+)/dates/new/(?P<result>\d+)/change/$', 'views.change_new_result'), #изменение статуса результата нового обохода
-    url(r'^checks/(?P<check>\d+)/dates/(?P<date>\d+)/delete/$', 'views.del_date'), #удаление даты обхода
-    url(r'^checks/(?P<check>\d+)/tasks/(?P<task>\d+)/delete/$', 'views.del_task'), #удаление задания обхода
-    url(r'^checks/(?P<check>\d+)/dates/new/save/$', 'views.save_date'), #сохранение даты обхода
-    url(r'^checks/(?P<check>\d+)/dates/cancel/$', 'views.cancel_date'), #удаление даты обхода
-    url(r'^checks/(?P<check>\d+)/generate/$', 'views.generate'), #тестовая генерация
-    url(r'^checks/(?P<check>\d+)/delete/$', 'views.del_check'), #удаление шаблона
+	url(r'^checks/$', 'views.view_checks'), #view template
+    url(r'^checks/(?P<check>\d+)/dates/$', 'views.view_dates'), #show check dates
+    url(r'^checks/(?P<check>\d+)/dates/new/$', 'views.new_date'), #create new check date
+    url(r'^checks/(?P<check>\d+)/tasks/$', 'views.view_tasks'), #edit template
+    url(r'^checks/(?P<check>\d+)/dates/(?P<date>\d+)/results/$', 'views.view_results'),  #show results of check date
+    url(r'^checks/(?P<check>\d+)/dates/(?P<date>\d+)/results/(?P<result>\d+)/change/$', 'views.change_result'), #change status of result
+    url(r'^checks/(?P<check>\d+)/dates/new/(?P<result>\d+)/change/$', 'views.change_new_result'), #change status of new check date result
+    url(r'^checks/(?P<check>\d+)/dates/(?P<date>\d+)/delete/$', 'views.del_date'), #delete check date
+    url(r'^checks/(?P<check>\d+)/tasks/(?P<task>\d+)/delete/$', 'views.del_task'), #delete template task
+    url(r'^checks/(?P<check>\d+)/dates/new/save/$', 'views.save_date'), #save new check date
+    url(r'^checks/(?P<check>\d+)/dates/cancel/$', 'views.cancel_date'), #cancel new check date
+    url(r'^checks/(?P<check>\d+)/generate/$', 'views.generate'), #generate 50 random dates for template
+    url(r'^checks/(?P<check>\d+)/delete/$', 'views.del_check'), #delete template
 	)
 
 urlpatterns += patterns('kulcalendar',
-    url(r'^checks/(?P<check>\d+)/calendar/(?P<year>\d+)/(?P<month>\d+)/$', 'views.view_as_calendar'), #вывод обходов в режиме календаря
+    url(r'^checks/(?P<check>\d+)/calendar/(?P<year>\d+)/(?P<month>\d+)/$', 'views.view_as_calendar'), #view check dates as calendar
     #url(r'^checks/(?P<check>\d+)/calendar/(?P<year>\d+)/(?P<month>\d+)/$', 'views.view_as_calendar_test'), #тестовый вывод
     )
